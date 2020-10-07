fa71b3d50b099ae94c170c7a88724e8aafbac85c
UsherbJGuay <72097783+UsherbJGuay@users.noreply.github.com>
2020-10-07 09:49:51 -0400

    do it

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,6 @@ void setup() {
 }
 
 void loop() {
-    delay(3000);
-    MOTOR_SetSpeed(0,0.35);
-    MOTOR_SetSpeed(1,0.35);
-    ENCODER_ReadReset(0);
-    ENCODER_ReadReset(1);
-    MOTOR_SetSpeed(0,0.75);
-    MOTOR_SetSpeed(1,0.75);
-    delay(1000);
-    MOTOR_SetSpeed(0,0);
-    MOTOR_SetSpeed(1,0);
-    while(1);
+    
 
 }
